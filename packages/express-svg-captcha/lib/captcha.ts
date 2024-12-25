076f9c460204e4783d4695410fd10c866ff40527
tikkhun <s565338107@163.com>
2024-12-25 16:13:25 +0800

    feat: charPresetIgnore

--- packages/express-svg-captcha/lib/captcha.ts
+++ packages/express-svg-captcha/lib/captcha.ts
@@ -1,12 +1,12 @@
 import svgCaptcha, { ConfigObject, CaptchaObj } from "svg-captcha-embed";
 export interface CaptchaCreateOptions extends ConfigObject {
   type: "text" | "mathExpr";
-  reChars: string;
+  charPresetIgnore: string;
 }
 export const captchaCreateDefaultOptions: CaptchaCreateOptions = Object.freeze({
   type: "text",
   size: 4, // 验证码长度
-  reChars: "0 o 1 i",
+  charPresetIgnore: "0o1i",
   noise: 2, // 干扰线条数
   color: true, // 验证码字符颜色
   background: "#fff", // 背景色
@@ -14,24 +14,31 @@ export const captchaCreateDefaultOptions: CaptchaCreateOptions = Object.freeze({
 export function createCaptcha(
   options?: Partial<typeof captchaCreateDefaultOptions>
 ): CaptchaObj {
-  const { type, reChars, ...opts } = Object.assign(
+  const { type, charPresetIgnore, ...opts } = Object.assign(
     captchaCreateDefaultOptions,
     options
   );
-  // 如果出现 0o1i等字样的重新
-  let data: any,
-    text: string = "";
-  while (!text || reChars.includes(text)) {
-    const value =
-      type === "text"
-        ? svgCaptcha.create(opts)
-        : svgCaptcha.createMathExpr(opts);
-    data = value.data;
-    text = value.text;
-  }
-  return { data, text };
-}
+  const charPreset =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const ignoredCharPreset = removeCharacters(charPreset, charPresetIgnore);
+  // 这里由于同时升成 svg 和 text 所以会有一定的性能消耗
+  const result =
+    type === "text"
+      ? svgCaptcha.create({ ...opts, charPreset: ignoredCharPreset })
+      : svgCaptcha.createMathExpr({ ...opts, charPreset: ignoredCharPreset });
 
+  return result;
+}
+export function removeCharacters(
+  original: string,
+  charactersToRemove: string
+): string {
+  // 创建一个正则表达式，匹配要删除的字符
+  const regex = new RegExp("[" + charactersToRemove + "]", "g");
+
+  // 使用正则表达式的replace方法删除所有匹配的字符
+  return original.replace(regex, "");
+}
 export interface CaptchaValidateOptions {
   captchaText?: string;
   captchaUserInput?: string;
